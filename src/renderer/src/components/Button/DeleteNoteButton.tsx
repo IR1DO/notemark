2839200785b62a2c4685c871b8b266f60177ec98
Kevin Hu <hellokevinhu@icloud.com>
2024-08-24 09:58:09 +0800

    Add delete and create functionality to Note buttons

--- src/renderer/src/components/Button/DeleteNoteButton.tsx
+++ src/renderer/src/components/Button/DeleteNoteButton.tsx
@@ -1,9 +1,17 @@
+import { deleteNoteAtom } from '@/store'
+import { useSetAtom } from 'jotai'
 import { ActionButton, ActionButtonProps } from './ActionButton'
 import { FaRegTrashCan } from 'react-icons/fa6'
 
 export const DeleteNoteButton = ({ ...props }: ActionButtonProps) => {
+  const deleteNote = useSetAtom(deleteNoteAtom)
+
+  const handleDelete = () => {
+    deleteNote()
+  }
+
   return (
-    <ActionButton {...props}>
+    <ActionButton onClick={handleDelete} {...props}>
       <FaRegTrashCan className="w-4 h-4 text-zinc-100" />
     </ActionButton>
   )
--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -1,6 +1,7 @@
 import { notesMock } from '@/store/mocks'
 import { NoteInfo } from '@shared/models'
 import { atom } from 'jotai'
+import { v4 as uuidv4 } from 'uuid'
 
 export const notesAtom = atom<NoteInfo[]>(notesMock)
 
@@ -20,4 +21,37 @@ export const selectedNoteAtom = atom((get) => {
     ...selectedNote,
     content: `Hello from Note${selectedNoteIndex}`
   }
+})
+
+export const createEmptyNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+
+  const title = `Note ${notes.length + 1}`
+
+  const newNote: NoteInfo = {
+    id: uuidv4(),
+    title,
+    lastEditTime: Date.now()
+  }
+
+  set(notesAtom, [newNote, ...notes])
+
+  set(selectedNoteIndexAtom, 0)
+})
+
+export const deleteNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+  const selectedNote = get(selectedNoteAtom)
+
+  if (!selectedNote) {
+    return null
+  }
+
+  set(
+    notesAtom,
+    notes.filter((note) => note.id !== selectedNote.id)
+  )
+
+  // don't have to return. just to pleasure the warning
+  return set(selectedNoteIndexAtom, null)
 })
