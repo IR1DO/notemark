73e5a84dfa7a8f0340d0d803fc026fa5b938ffcb
Kevin Hu <hellokevinhu@icloud.com>
2024-08-25 09:40:06 +0800

    Implement scrolling to new note position after creation

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -13,8 +13,16 @@ import Scrollbars from 'react-custom-scrollbars-2'
 
 const App = () => {
   const markdownEditorRef = useRef<Scrollbars>(null)
-  const resetScroll = () => {
+  const resetMarkdownEditorScroll = () => {
     markdownEditorRef.current?.scrollToTop()
+  }
+
+  const notePreviewListRef = useRef<Scrollbars>(null)
+  const scrollToNote = (id: string) => {
+    const noteElement = document.getElementById(id)
+    if (notePreviewListRef.current && noteElement) {
+      notePreviewListRef.current.scrollTop(noteElement.offsetTop)
+    }
   }
 
   return (
@@ -23,7 +31,12 @@ const App = () => {
       <RootLayout className="bg-zinc-600/50">
         <Sidebar className="p-2 flex flex-col gap-2 overflow-hidden">
           <ActionButtonsRow className="flex justify-between mt-1" />
-          <NotePreviewList className="space-y-1" onSelect={resetScroll} />
+          <NotePreviewList
+            ref={notePreviewListRef}
+            className="space-y-1"
+            onSelect={resetMarkdownEditorScroll}
+            scrollToNote={scrollToNote}
+          />
         </Sidebar>
         <Content className="p-2 border-l bg-zinc-900/50 border-l-white/20 flex flex-col overflow-hidden">
           <FloatingNoteTitle />
--- src/renderer/src/components/NotePreviewList.tsx
+++ src/renderer/src/components/NotePreviewList.tsx
@@ -1,37 +1,49 @@
 import { NotePreview } from '@/components'
-import { ComponentProps } from 'react'
+import { ComponentProps, forwardRef, useEffect } from 'react'
 import { twMerge } from 'tailwind-merge'
 import Scrollbars from 'react-custom-scrollbars-2'
 import { useNotesList } from '@renderer/hooks/useNotesList'
 
 export type NotePreviewListProps = ComponentProps<'ul'> & {
-  onSelect?: () => void
+  onSelect: () => void
+  scrollToNote: (id: string) => void
 }
 
-export const NotePreviewList = ({ onSelect, className, ...props }: NotePreviewListProps) => {
-  const { notes, selectedNoteIndex, handleNoteSelect } = useNotesList({ onSelect })
+export const NotePreviewList = forwardRef<Scrollbars, NotePreviewListProps>(
+  ({ onSelect, scrollToNote, className, ...props }, ref) => {
+    const { notes, selectedNoteIndex, handleNoteSelect } = useNotesList({ onSelect })
 
-  if (notes.length === 0) {
+    if (notes.length === 0) {
+      return (
+        <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={500}>
+          <ul className={twMerge('text-center pt-4', className)} {...props}>
+            <span>No Notes Yet!</span>
+          </ul>
+        </Scrollbars>
+      )
+    }
+
+    useEffect(() => {
+      if (selectedNoteIndex) {
+        scrollToNote(notes[selectedNoteIndex].id)
+      }
+    }, [selectedNoteIndex])
+
     return (
-      <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={500}>
-        <ul className={twMerge('text-center pt-4', className)} {...props}>
-          <span>No Notes Yet!</span>
+      <Scrollbars ref={ref} autoHide autoHideTimeout={1000} autoHideDuration={500}>
+        <ul className={className} {...props}>
+          {notes.map((note, index) => (
+            <NotePreview
+              key={note.id}
+              isActive={selectedNoteIndex === index}
+              onClick={handleNoteSelect(index)}
+              {...note}
+            />
+          ))}
         </ul>
       </Scrollbars>
     )
   }
-  return (
-    <Scrollbars autoHide autoHideTimeout={1000} autoHideDuration={500}>
-      <ul className={className} {...props}>
-        {notes.map((note, index) => (
-          <NotePreview
-            key={note.id}
-            isActive={selectedNoteIndex === index}
-            onClick={handleNoteSelect(index)}
-            {...note}
-          />
-        ))}
-      </ul>
-    </Scrollbars>
-  )
-}
+)
+
+NotePreviewList.displayName = 'NotePreviewList'
