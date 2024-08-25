2d3ab79e0f8ff27d104fab98452459470e477ca0
Kevin Hu <hellokevinhu@icloud.com>
2024-08-25 11:27:25 +0800

    Change time zone of date formatter

--- src/renderer/src/utils/index.ts
+++ src/renderer/src/utils/index.ts
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge'
 const dateFormatter = new Intl.DateTimeFormat(window.context.locale, {
   dateStyle: 'short',
   timeStyle: 'short',
-  timeZone: 'UTC'
+  timeZone: 'Asia/Shanghai'
 })
 
 export const formatDateFormMs = (ms: number) => dateFormatter.format(ms)
